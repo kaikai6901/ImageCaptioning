2bf0cc159f1ba8a8286cfe6e5df8d768120226d0
kaikai6901 <huyhoangbk64@gmail.com>
2023-02-08 04:12:13 +0700

    fix config

--- utils/helper.py
+++ utils/helper.py
@@ -111,13 +111,5 @@ class Helper:
         return dataset, tokenizer   
     # store the features to a numpy file
 
-def create_look_ahead_mask(sequence_length):
-    mask = tf.linalg.band_part(tf.ones((1, sequence_length, sequence_length)), -1, 0)
-    return mask 
-
-def create_padding_mask(decoder_token_ids):
-    seq = 1 - tf.cast(tf.math.equal(decoder_token_ids, 0), tf.float32)
-  
-    return seq[:, tf.newaxis, :]
 
 
